dd7af2b80a60793810018deb4e1425091603aab3
Brittany Harrington <bharrington@quantaservices.com>
2018-01-04 17:32:09 -0600

    Fixed duplicate code line

--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,3 @@
-
 import Vue from 'vue'
 import Vuex from 'vuex'
 import createPersistedState from 'vuex-persistedstate'
@@ -12,34 +11,33 @@ import worker from './worker.js'
 import phone from './phone.js'
 import publicModule from './public.js'
 import map from './map.js'
-import phone from './phone.js'
 
 // https://github.com/robinvdvleuten/vuex-persistedstate
 const persistedStateOptions = {
-  paths: ['auth', 'worker']
+    paths: ['auth', 'worker']
 };
 
 const VuexLoading = createVuexLoader({
-  // The Vuex module name, 'loading' by default.
-  moduleName: 'loading',
-  // The Vue component name, 'v-loading' by default.
-  componentName: 'v-loading',
-  // Vue component class name, 'v-loading' by default.
-  className: 'v-loading',
+    // The Vuex module name, 'loading' by default.
+    moduleName: 'loading',
+    // The Vue component name, 'v-loading' by default.
+    componentName: 'v-loading',
+    // Vue component class name, 'v-loading' by default.
+    className: 'v-loading',
 });
 
 Vue.use(VuexLoading);
 
 const store = new Vuex.Store({
-  modules: {
-    auth,
-    worker,
-    publicModule,
-    map,
-    phone
-  },
-  strict: process.env.NODE_ENV !== 'production',
-  plugins: [VuexLoading.Store, createPersistedState(persistedStateOptions)]
+    modules: {
+        auth,
+        worker,
+        publicModule,
+        map,
+        phone
+    },
+    strict: process.env.NODE_ENV !== 'production',
+    plugins: [VuexLoading.Store, createPersistedState(persistedStateOptions)]
 });
 
 export default store
