a2b70d7448951bd2fc3de0b511989b3e20b131d0
Kira Jones <kirajones@KJONES-MBP-HOU.local>
2018-01-17 18:22:47 -0600

    Adding delete/add UI front end for articles, not hooked up to the backend yet.

--- src/views/phone/GetUpToDate.vue
+++ src/views/phone/GetUpToDate.vue
@@ -1,25 +1,65 @@
 <template>
     <div>
-     <h2> Latest Important News </h2>
-     <div v-if="articles.length > 0">
-       <div v-for="article in articles" v-bind:key="article.id">
-            <input type="checkbox" v-model="article.isRead"> <b>{{article.title}}</b>
-            <div>{{article.description}}</div>        
+      <div class="card">
+        <div class="card-header text-center">Latest Crisis Cleanup News 
+          <!-- TODO: Hide for admins only -->
+          <button class="float-right" @click="toggleEditMode"><i class="icon-settings"></i></button>
         </div>
-    </div>
-    <br>
-    <div v-if="allArticlesRead">    
-      Congratulations! You're up to date with the latest news.
-    </div>
-    <br>
-    <button type="button" class="btn-block btn-primary" @click="goBack">Submit</button>
-    </div>
+            <div class="card-body" >
+              <p class="card-text">
+                <div v-if="articles.length > 0">
+                  <div v-for="article in articles.filter(x => !x.isDeleted)" v-bind:key="article.id">
+                      <input type="checkbox" v-model="article.isRead"  v-if="!inEditMode"> 
+                      <b>{{article.title}}</b>
+                      <!--Show edit mode buttons-->
+                      <span class="inline-block btn-group" v-if="inEditMode"> 
+                          <button class="btn btn-primary" @click="article.isDeleted = true"><i class="icon-trash"></i></button>
+                      </span>
+                      <div>{{article.description}}</div>        
+                      <br>
+                  </div>
+                  </div>
+                  <div v-else>
+                    No news
+                  </div>
+                  <button class="btn btn-success" v-b-modal.articleModal v-if="inEditMode"><i class="icon-plus"></i></button>
+                  <br><br>
+                  <div class="alert alert-success" role="alert" v-if="allArticlesRead && !inEditMode">
+                    <strong>Congratulations! </strong>You're up to date with the latest news.
+                  </div>
+                  <button type="button" class="btn btn-primary" @click="save">Save</button>
+              </p>
+          </div>
+        </div>
+
+      <!-- Add article modal -->
+      <b-modal id="articleModal" title="New Article" centered @ok="articleModalOk" ref="modal" hide-footer>
+        <b-form @submit.stop.prevent="addArticle">
+        <b-form-input type="text"
+                      placeholder="Title"
+                      required
+                      v-model="newArticle.title"></b-form-input>
+        <br>
+        <b-form-textarea placeholder="Description"
+                      required
+                      v-model="newArticle.description"
+                      :rows="3"></b-form-textarea>
+          <br>
+          <b-button type="submit" variant="primary">Save</b-button>
+        </b-form>
+
+      </b-modal>
+
+      </div>
 </template>
 <script>
   export default {
       data() {
         return {
+          newArticle : {title:null, description:null, isRead: false, isDeleted: false},
+          inEditMode: false,
           articles: [],
+          originalArticles: [],
           allArticlesRead: false
       }
     },
@@ -39,19 +79,52 @@
                 return readArticle === article.id;
               });
               vm.$set(article, 'isRead', articleFound === undefined ? false : true);
+              vm.$set(article, 'isDeleted', false);
             });
             console.log(this.articles);
         });
       },
-      goBack(){
-        //Update the user's read article list
+      articleModalOk (evt) {
+        // Prevent modal from closing
+        evt.preventDefault()
+        //this.$refs.form1.submit()
+        this.addArticle(evt)
+      },
+      addArticle(evt){
+        console.log(evt)
+        evt.preventDefault()
+        //alert(JSON.stringify(this.form))
+        this.articles.push(Object.assign({}, this.newArticle));
+        //Clear new article
+        this.newArticle.title = null;
+        this.newArticle.description = null;
+        this.$refs.modal.hide()
+      },
+      toggleEditMode(){
+        this.inEditMode = !this.inEditMode
+        if(this.inEditMode) {
+          //Store original articles in-case user cancels
+          this.originalArticles = JSON.parse(JSON.stringify(this.articles))
+        } else {
+          //Otherwise if they are toggleing out revert to originals
+          //TODO: Warn user they will lose their changes
+          this.articles = this.originalArticles;
+        }
+      },
+      save(){
         var vm = this;
-        var read_article_ids = this.articles.filter(article => article.isRead).map(article => article.id);
-        this.$http.post(`${process.env.API_PHONE_ENDPOINT}/users/` + vm.$store.state.phone.caller.id + `/set_read_articles`, read_article_ids).then(r => {
-          this.$router.go(-1);
-        }).catch(err => {
-          console.log(err)
-        });
+        if(this.inEditMode) {
+          //TODO: Save changes and retrieve new articles
+          this.inEditMode = false;
+        } else {
+          //Update the user's read article list
+          var read_article_ids = this.articles.filter(article => article.isRead).map(article => article.id);
+          this.$http.post(`${process.env.API_PHONE_ENDPOINT}/users/` + vm.$store.state.phone.caller.id + `/set_read_articles`, read_article_ids).then(r => {
+            this.$router.go(-1);
+          }).catch(err => {
+            console.log(err)
+          });
+        }
       }
     },
     watch: {
