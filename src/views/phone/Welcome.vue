bd8783f48977a3d77cbdd2e023da411ce0c47285
Kira Jones <kirajones@KJONES-MBP-HOU.local>
2018-01-25 12:25:27 -0600

    Fixing formatting of the phone welcome page.

--- src/views/phone/Welcome.vue
+++ src/views/phone/Welcome.vue
@@ -1,53 +1,61 @@
 <template>
-    <div>
-    <div class="row">
-
+  <div>
+    <b-container fluid>
+    <b-row>
        <!-- ******************** Check-list ********************* -->
-    <div class="col-md-6">
-          <div class="col-md-6">
-            <button type="button" class="btn btn-block btn-primary" @click="getTrained"><i class="icon-check float-left" v-if="user.is_trained" style="font-size:18pt"></i><i class="icon-exclamation float-left" v-else style="font-size:18pt"></i>Get Trained</button>
-            <button type="button" class="btn btn-block btn-primary" @click="getUpToDate"><i class="icon-check float-left" v-if="user.is_up_to_date" style="font-size:18pt"></i><i class="icon-exclamation float-left" v-else style="font-size:18pt"></i>Get Up To Date</button>
-            <button type="button" class="btn btn-block btn-primary" @click="getStarted">Get Started</button>
-          </div>   
-      </div>
-      <!-- ******************** Call center experts ********************* --> 
+      <b-col md="4">
+        <b-card no-body header="Getting Started">
+          <b-list-group flush>
+            <b-list-group-item @click="getTrained" href="#">Get Trained
+              <i class="icon-check float-right" v-if="user.is_training_completed" style="font-size:18pt"></i><i class="icon-exclamation float-right" v-else style="font-size:18pt"></i>
+            </b-list-group-item>
+            <b-list-group-item @click="getUpToDate" href="#">Get Up To Date
+              <i class="icon-check float-right" v-if="user.is_up_to_date" style="font-size:18pt"></i><i class="icon-exclamation float-right" v-else style="font-size:18pt"></i>      
+            </b-list-group-item>      
+            <b-list-group-item @click="getStarted" href="#">Get Started
+              <i class="icon-control-play float-right" style="font-size:18pt"></i>
+            </b-list-group-item>
+          </b-list-group>
+        </b-card>
+      </b-col>
 
-      <div class="col-lg-6"  align="right">
-          <div class="card">
-            <div class="card-header text-center">Ask a Call Center Expert</div>
-            <div class="card-body" >
-              <p class="card-text">
-              <table style="width:100%">
-                <tr v-for="expert in callCenterExperts" v-bind:key="expert.id">
-                    <td style="width:50%">{{ expert.name }}</td>
-                    <td style="width:50%">{{ expert.number }}</td>
-                </tr>
-              </table>
-              </p>
-            </div>
+      <!-- ******************** Call center experts ********************* --> 
+      <b-col md="8" offset-md="4">
+        <div class="card">
+          <div class="card-header text-center">Ask a Call Center Expert</div>
+          <div class="card-body" >
+            <p class="card-text">
+            <table style="width:100%">
+              <tr v-for="expert in callCenterExperts" v-bind:key="expert.id">
+                  <td style="width:50%">{{ expert.name }}</td>
+                  <td style="width:50%">{{ expert.number }}</td>
+              </tr>
+            </table>
+            </p>
           </div>
-      </div>
-</div>
-    
+        </div>
+      </b-col>
+    </b-row>
 
     <!-- ******************** Call center planning  ********************* -->
+    <b-card no-body>
+        <b-tabs card>
+          <b-tab title="Schedule" active>
+            <br>
+            <a href="https://api.sling.is/" target="_blank"> 
+              <img src="/static/img/front_end/logo-sling.jpg" alt="Sling" img height="100">
+            </a>
+          </b-tab>
+          <b-tab title="Hours" >
+            <br>A beautiful hours report
+          </b-tab>
+          <b-tab title="Stats">
+            <br>Stats here
+          </b-tab>
+        </b-tabs>
+      </b-card>
 
-    <b-tabs>
-      <b-tab title="Schedule" active>
-        <br>
-        <a href="https://api.sling.is/" target="_blank"> 
-          <img src="/static/img/front_end/logo-sling.jpg" alt="Sling" img height="100">
-        </a>
-      </b-tab>
-      <b-tab title="Hours" >
-        <br>A beautiful hours report
-      </b-tab>
-      <b-tab title="Stats">
-        <br>Stats here
-      </b-tab>
-    </b-tabs>
-
-
+    </b-container>
     </div>
 </template>
 
