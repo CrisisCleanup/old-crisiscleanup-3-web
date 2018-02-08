76d046223fd526e4145312d09380ddaaef0d0f0f
Tyler Sanders <tylerssanders@gmail.com>
2018-02-08 13:27:57 -0600

    form engine

--- src/views/worker/MyOrganization.vue
+++ src/views/worker/MyOrganization.vue
@@ -1,16 +1,18 @@
 <template>
   <div class="animated fadeIn">
-    <WorksiteDatatable></WorksiteDatatable>
+    <!--<WorksiteDatatable></WorksiteDatatable>-->
+    <EventForm />
   </div>
 </template>
 
 <script>
   import WorksiteDatatable from '@/components/datatables/WorksiteDatatable';
+  import EventForm from '@/components/form/EventForm';
 
   export default {
     name: 'MyOrganization',
     components: {
-      WorksiteDatatable
+      EventForm
     }
   }
 </script>
