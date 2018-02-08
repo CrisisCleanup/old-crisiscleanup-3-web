c9a893b7974fd1ff420a12b9f5ebc50066b10377
Tyler Sanders <tylerssanders@gmail.com>
2018-02-08 14:06:38 -0600

    text field

--- src/components/form/FormSection.vue
+++ src/components/form/FormSection.vue
@@ -2,12 +2,35 @@
   <section>
     <h1>{{ titleLabel }}</h1>
     <div v-for="(value, key) in data.fields">
-      {{key}}
+      <!--{{key}}-->
+      <div v-if="value.field_type=='text'">
+        <!--{{key}} - text-->
+        <TextField :label-t="value.label_t"></TextField>
+      </div>
+      <div v-else-if="value.field_type=='hidden'">
+        {{key}} - hidden
+      </div>
+      <div v-else-if="value.field_type=='textarea'">
+        {{key}} - textarea
+      </div>
+      <div v-else-if="value.field_type=='select'">
+        {{key}} - select
+      </div>
+      <div v-else-if="value.field_type=='multiselect'">
+        {{key}} - multiselect
+      </div>
+      <div v-else-if="value.field_type=='checkbox'">
+        {{key}} - checkbox
+      </div>
+      <div v-else-if="value.field_type=='radio'">
+        {{key}} - checkbox
+      </div>
     </div>
   </section>
 </template>
 
 <script>
+import TextField from './TextField.vue';
 export default {
   props: {
     titleLabel: {
@@ -27,6 +50,9 @@ export default {
     },
   },
   computed: {
+  },
+  components: {
+    TextField
   }
 }
 </script>
---
+++ src/components/form/TextField.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="form-group">
+    <label>{{ $t(labelT) }}<span data-tooltip aria-haspopup="true"
+                                                                      class="has-tip tip-bottom"
+                                                                      title="This may be the primary contact, resident, or owner.">
+      <i class="fa fa-question"></i>
+    </span>
+    </label>
+    <input class="form-control" type="text" id="legacy_legacy_site_name"/>
+    <!--<div v-show="siteFormErrors.name" class="text-danger">{{siteFormErrors.name ? siteFormErrors.name[0] : ''}}</div>-->
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      labelName: {
+        type: String
+      },
+      inputValue: {
+        type: Object
+      },
+      isRequired: {
+        type: Boolean
+      },
+      validation: {
+        type: String
+      },
+      labelT: {
+        type: String
+      },
+      helpTextT: {
+        type: String
+      },
+      isRequired: {
+        type: Boolean
+      },
+      isReadonly: {
+        type: Boolean
+      },
+      allowEditBreakGlass: {
+        type: Boolean
+      },
+      allowToggleHiding: {
+        type: Boolean
+      },
+      isHiddenDefault: {
+        type: Boolean
+      },
+      ifSelectedThenWorkType: {
+        type: Boolean
+      },
+      placeholderT: {
+        type: String
+      },
+    },
+    computed: {}
+  }
+</script>
