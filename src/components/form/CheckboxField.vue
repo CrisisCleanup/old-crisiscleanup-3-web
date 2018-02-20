a472532d00c92f852a800034d4d0f4cfdfca451e
Tyler Sanders <tylerssanders@gmail.com>
2018-02-19 21:39:05 -0600

    Fields

---
+++ src/components/form/CheckboxField.vue
@@ -0,0 +1,59 @@
+<template>
+  <div>
+    <p></p>
+    <div class="form-check">
+      <label class="form-check-label">
+        <input class="form-check-input" type="checkbox" value="">
+        {{ $t(labelT) }}
+      </label>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      labelName: {
+        type: String
+      },
+      inputValue: {
+        type: Object
+      },
+      validation: {
+        type: String
+      },
+      labelT: {
+        type: String
+      },
+      helpTextT: {
+        type: String
+      },
+      isRequired: {
+        type: Boolean
+      },
+      isReadonly: {
+        type: Boolean
+      },
+      allowEditBreakGlass: {
+        type: Boolean
+      },
+      allowToggleHiding: {
+        type: Boolean
+      },
+      isHiddenDefault: {
+        type: Boolean
+      },
+      ifSelectedThenWorkType: {
+        type: Boolean
+      },
+      placeholderT: {
+        type: String
+      },
+      options: {
+        type: Array
+      }
+    },
+    computed: {}
+
+  }
+</script>
---
+++ src/components/form/TextAreaField.vue
@@ -0,0 +1,60 @@
+<template>
+  <div>
+    <div class="">
+      <label class="">{{ $t(labelT) }}
+        <span v-if="helpTextT !== null" data-tooltip aria-haspopup="true"
+              class="has-tip tip-bottom"
+              :title="helpTextT">
+      <i class="fa fa-question"></i>
+    </span>
+      </label>
+      <textarea class="form-control"></textarea>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      labelName: {
+        type: String
+      },
+      inputValue: {
+        type: Object
+      },
+      validation: {
+        type: String
+      },
+      labelT: {
+        type: String
+      },
+      helpTextT: {
+        type: String
+      },
+      isRequired: {
+        type: Boolean
+      },
+      isReadonly: {
+        type: Boolean
+      },
+      allowEditBreakGlass: {
+        type: Boolean
+      },
+      allowToggleHiding: {
+        type: Boolean
+      },
+      isHiddenDefault: {
+        type: Boolean
+      },
+      ifSelectedThenWorkType: {
+        type: Boolean
+      },
+      placeholderT: {
+        type: String
+      },
+    },
+    computed: {}
+
+//onclick="document.get('legacy_legacy_site_request_date').readOnly=false; document.getElementById('legacy_legacy_site_request_date')//.focus(); return false;"
+  }
+</script>
