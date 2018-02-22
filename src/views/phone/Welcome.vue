2706d9402795f9405575d690a4206a996769d8c7
Kira Jones <kirajones@KJONES-MBP-HOU.local>
2018-02-22 17:45:58 -0600

    Disabling start button when any training or articles have not been completed. Fixing bug with adding questions to a training module duplicating the entire module.

--- src/views/phone/Welcome.vue
+++ src/views/phone/Welcome.vue
@@ -11,8 +11,12 @@
             </b-list-group-item>
             <b-list-group-item @click="getUpToDate" href="#">Get Up To Date
               <i class="icon-check float-right" v-if="user.is_up_to_date" style="font-size:18pt"></i><i class="icon-exclamation float-right" v-else style="font-size:18pt"></i>      
-            </b-list-group-item>      
-            <b-list-group-item @click="getStarted" href="#">Get Started
+            </b-list-group-item>   
+            <h1 v-if="ok">Yes</h1>
+            <b-list-group-item @click="getStarted" href="#" v-if="user.is_training_completed && user.is_up_to_date">Get Started
+              <i class="icon-control-play float-right" style="font-size:18pt"></i>
+            </b-list-group-item>
+            <b-list-group-item @click="getStarted" href="#" v-else disabled>Get Started
               <i class="icon-control-play float-right" style="font-size:18pt"></i>
             </b-list-group-item>
           </b-list-group>
@@ -60,78 +64,87 @@
 </template>
 
 <script>
-  import Vue from 'vue'
-  import {mapGetters} from 'vuex'
+import Vue from "vue";
+import { mapGetters } from "vuex";
 
-  export default {
-      data() {
-        return {
-          callCenterExperts: [],
-          stories: [
-            { description: "xyz"},
-            { description: "xyz"}
-          ]
-      }
-    },
-    mounted() {
-    },
-    created: function() {
-      this.getUserData();
-      this.getCallExperts();
-    },
-    computed: {
-      ...mapGetters('auth', {
-        userId: 'getUserId'
-      }),
-      ...mapGetters('phone', {
-        user: 'getUser',
-        gateway: 'getGateway'
-      })
-    },
-    methods: {
-      async getUserData(){
-        //Grab user information
-        this.$store.dispatch('phone/getUserDetails', {userId: this.userId, overwrite: true}).then(() => {
-            this.getGatewayData();
+export default {
+  data() {
+    return {
+      callCenterExperts: [],
+      stories: [{ description: "xyz" }, { description: "xyz" }]
+    };
+  },
+  mounted() {},
+  created: function() {
+    this.getUserData();
+    this.getCallExperts();
+  },
+  computed: {
+    ...mapGetters("auth", {
+      userId: "getUserId"
+    }),
+    ...mapGetters("phone", {
+      user: "getUser",
+      gateway: "getGateway"
+    })
+  },
+  methods: {
+    async getUserData() {
+      //Grab user information
+      this.$store
+        .dispatch("phone/getUserDetails", {
+          userId: this.userId,
+          overwrite: true
+        })
+        .then(() => {
+          this.getGatewayData();
         });
-      },
-      async getGatewayData(){
-        //Get user's current gateway info
-        if (this.user.gateway != null) {
-          await this.$store.dispatch('phone/getGatewayDetails', this.user.gateway).catch(err => {
+    },
+    async getGatewayData() {
+      //Get user's current gateway info
+      if (this.user.gateway != null) {
+        await this.$store
+          .dispatch("phone/getGatewayDetails", this.user.gateway)
+          .catch(err => {
             console.log(err);
           });
-        } else {
-          //TODO: add default gateway
-          console.log("no gateway specified for user");
-        }
-      },
-      getCallExperts(){
-        //Grab the call expert ids/numbers
-        this.$http.get(`${process.env.API_PHONE_ENDPOINT}/users?willing_to_be_call_center_support=true`).then(r => {
+      } else {
+        //TODO: add default gateway
+        console.log("no gateway specified for user");
+      }
+    },
+    getCallExperts() {
+      //Grab the call expert ids/numbers
+      this.$http
+        .get(
+          `${process.env
+            .API_PHONE_ENDPOINT}/users?willing_to_be_call_center_support=true`
+        )
+        .then(r => {
           var callExperts = r.data.results;
           console.log(callExperts);
-          if(callExperts != null && callExperts.length > 0) {
+          if (callExperts != null && callExperts.length > 0) {
             callExperts.forEach(function(expert) {
-              expert.number = expert.last_used_phone_number
-              expert.name = expert.name
-            })
+              expert.number = expert.last_used_phone_number;
+              expert.name = expert.name;
+            });
             this.callCenterExperts = callExperts;
           }
-        }).catch(err => {
-          console.log(err)
+        })
+        .catch(err => {
+          console.log(err);
         });
-      },
-      getUpToDate() {
-        this.$router.push({ path: 'News' });
-      },
-      getTrained() {
-        this.$router.push({ path: 'Training' });
-      },
-      getStarted() {
-        this.$store.commit('phone/seenWelcome');
-        this.$router.push({ name: 'Phone' });
-      }
+    },
+    getUpToDate() {
+      this.$router.push({ path: "News" });
+    },
+    getTrained() {
+      this.$router.push({ path: "Training" });
+    },
+    getStarted() {
+      this.$store.commit("phone/seenWelcome");
+      this.$router.push({ name: "Phone" });
     }
   }
+};
 </script>
