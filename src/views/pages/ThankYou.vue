170ec5762902a123f2a4e08c2070f21536d5c91c
Aaron Titus <aaron@crisiscleanup.org>
2018-03-20 01:44:45 -0600

    Translate Thank You Page

--- src/views/pages/ThankYou.vue
+++ src/views/pages/ThankYou.vue
@@ -4,16 +4,10 @@
       <div class="row justify-content-center">
         <div class="col-md-6">
           <div class="clearfix">
-            <h1 class="float-left display-3 mr-4">This is the Thank You Page</h1>
-            <h4 class="pt-3">{{ $t('fiveHundred.houston_problem') }}</h4>
-            <p class="text-muted">{{ $t('fiveHundred.page_not_available_msg') }}</p>
-          </div>
-          <div class="input-prepend input-group">
-            <span class="input-group-addon"><i class="fa fa-search"></i></span>
-            <input id="prependedInput" class="form-control" size="16" type="text" v-bind:placeholder = "$t('fiveHundred.looking_for')">
-            <span class="input-group-btn">
-              <button class="btn btn-info" type="button">{{ $t('actions.search') }}</button>
-            </span>
+            <h1 class="float-left display-3 mr-4">{{ $t('thankYou.thank_you_donation') }}</h1>
+            <h4 class="pt-3">{{ $t('thankYou.impact_statement') }}</h4>
+            <h4 class="pt-3">{{ $t('thankYou.impact_statement_2') }}</h4>
+            <p class="text-muted">{{ $t('thankYou.return_to_map') }}</p>
           </div>
         </div>
       </div>
