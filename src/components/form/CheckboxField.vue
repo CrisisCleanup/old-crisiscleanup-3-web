181320f5347dbdda002fdc8f527edeb044883b85
Carter Sanders <carterwsanders@gmail.com>
2018-04-16 20:42:00 -0500

    Unit tests for the components/form.

--- src/components/form/CheckboxField.vue
+++ src/components/form/CheckboxField.vue
@@ -2,9 +2,10 @@
   <div class="form-check">
     <label class="form-check-label">
       <input class="form-check-input" type="checkbox" value="" required="isRequired">
-      <span v-if="isRequired">*</span>{{ $t(labelT) }}
-      <span v-if="helpTextT !== null" v-b-tooltip.hover aria-haspopup="true"
+      <span ref="isRequiredAsterisk" v-if="isRequired">*</span>{{ $t(labelT) }}
+      <span v-if="helpTextT" v-b-tooltip.hover aria-haspopup="true"
             class="has-tip tip-bottom"
+            ref="helpTextSpan"
             :title="$t(helpTextT)">
           <i class="fa fa-question"></i>
         </span>
--- test/unit/specs/components/form/SelectField.spec.js
+++ test/unit/specs/components/form/SelectField.spec.js
@@ -1,4 +1,4 @@
-import TextField from '@/components/form/TextField';
+import SelectField from '@/components/form/SelectField';
 import {shallow, createLocalVue} from '@vue/test-utils';
 import i18n from '@/services/i18n';
 import BootstrapVue from 'bootstrap-vue'
@@ -7,10 +7,10 @@ import sinon from "sinon";
 const localVue = createLocalVue();
 localVue.use(BootstrapVue);
 
-describe('TextField.vue', function () {
+describe('SelectField.vue', function () {
 
   const createBaseWrapper = (propsData = {}) => {
-    return shallow(TextField, {
+    return shallow(SelectField, {
       localVue,
       i18n,
       propsData
@@ -49,43 +49,11 @@ describe('TextField.vue', function () {
     expect(wrapper.find({ref: 'helpTextSpan'}).exists()).to.be.false;
   });
 
-  it('should show allow break edit glass button', ()=> {
-    const wrapper = createBaseWrapper({
-      allowEditBreakGlass: true
-    });
-    expect(wrapper.find({ref: 'allowEditLink'}).exists()).to.be.true;
-  });
-
-  it('should NOT show allow break edit glass button', ()=> {
-    const wrapper = createBaseWrapper({
-      //allowEditBreakGlass: false
-    });
-    expect(wrapper.find({ref: 'allowEditLink'}).exists()).to.be.false;
-  });
-
-  it('should enable the input text field when break glass edit button is enabled and clicked', function() {
-  const wrapper = createBaseWrapper({
-    isReadonly: true,
-    allowEditBreakGlass: true
-  });
-  // input field is readonly
-  // 'edit' button/link is clicked
-  wrapper.find('a').trigger('click');
-  // clicking the 'edit' button will flip the state of 'localIsReadonly'
-  // in this case to false, which will cause the input field to be readable
-  expect(wrapper.vm.$data.localIsReadonly).to.be.false;
-
-  // the opposite state gets toggled here and the input field will switch back
-  // to being readonly
-  wrapper.find('a').trigger('click');
-  expect(wrapper.vm.$data.localIsReadonly).to.be.true;
-});
-
   it('should mark the input field readonly if isReadonly prop is true', () => {
   const wrapper = createBaseWrapper({
     isReadonly: true
   });
-  expect(wrapper.find('input').attributes()['readonly']).to.equal('readonly');
+  console.log(wrapper.find('input').attributes()['readonly']).to.equal('readonly');
 
 });
 
