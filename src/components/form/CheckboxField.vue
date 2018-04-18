e8b2b66242143a9553d08fc7ff245af5c8b3bbc4
Carter Sanders <carterwsanders@gmail.com>
2018-04-17 20:55:11 -0500

    More form tests.

--- src/components/form/CheckboxField.vue
+++ src/components/form/CheckboxField.vue
@@ -1,16 +1,16 @@
 <template>
   <div class="form-check">
     <label class="form-check-label">
-      <input class="form-check-input" type="checkbox" required="isRequired"
-            :checked="isChecked" @change="(event) => toggleCheck(event)">
-      <span v-if="isRequired">*</span>{{ $t(labelT) }}
-      <span v-if="helpTextT !== null" v-b-tooltip.hover aria-haspopup="true"
+      <span ref="isRequiredAsterisk" v-if="isRequired">*</span>{{ $t(labelT) }}
+      <span v-if="helpTextT" v-b-tooltip.hover aria-haspopup="true"
             class="has-tip tip-bottom"
             ref="helpTextSpan"
             :title="$t(helpTextT)">
           <i class="fa fa-question"></i>
         </span>
     </label>
+    <input class="form-check-input" type="checkbox" required="isRequired"
+          :checked="isChecked" @change="(event) => toggleCheck(event)">
   </div>
 </template>
 
