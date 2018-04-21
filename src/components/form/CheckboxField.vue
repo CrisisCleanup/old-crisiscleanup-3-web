e1c448730139ae557c86264792957c568fb73330
Tyler Sanders <tylerssanders@gmail.com>
2018-04-21 09:38:50 -0500

    Flip checkbox back around

--- src/components/form/CheckboxField.vue
+++ src/components/form/CheckboxField.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="form-check">
     <label class="form-check-label">
+      <input class="form-check-input" type="checkbox" required="isRequired"
+             :checked="isChecked" @change="(event) => toggleCheck(event)">
       <span ref="isRequiredAsterisk" v-if="isRequired">*</span>{{ $t(labelT) }}
       <span v-if="helpTextT" v-b-tooltip.hover aria-haspopup="true"
             class="has-tip tip-bottom"
@@ -9,8 +11,6 @@
           <i class="fa fa-question"></i>
         </span>
     </label>
-    <input class="form-check-input" type="checkbox" required="isRequired"
-          :checked="isChecked" @change="(event) => toggleCheck(event)">
   </div>
 </template>
 
