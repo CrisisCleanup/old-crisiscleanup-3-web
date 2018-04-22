071ac290cbda262a0c3dd34d35db32a927bb6bac
Carter Sanders <carterwsanders@gmail.com>
2018-04-21 23:44:06 -0500

    Notification updates.

--- src/components/aside/worksite/WorksiteControls.vue
+++ src/components/aside/worksite/WorksiteControls.vue
@@ -1,79 +1,115 @@
 <template>
-  <div >
-    <div class="row">
-      <div class="col">
-        <div class="mx-auto" role="group">
-          <button id="newSiteBtn" @click="enterNewSite" class="btn btn-primary">{{ $t('actions.new') }}</button>
-          <button type="button" v-show="getWorksiteViews && getWorksiteViews.editWorksite" @click="saveForm" id="save-btn-top" class="btn btn-secondary">{{ $t('actions.save') }}</button>
-          <!--<b-btn id="printBtn" v-b-modal.modal1>Print</b-btn>-->
-          <button id="claim-btn" @click="fireClaimBtn" class="btn btn-secondary"
-                  v-show="isCurrentSiteClaimedByUserOrg || !isCurrentSiteClaimed"
-                  v-text="!isCurrentSiteClaimed || isCurrentSiteClaimed === null ? $t('actions.claim') : $t('actions.unclaim')"></button>
-          <!--<button id="historyBtn" v-show="getWorksiteViews && getWorksiteViews.editWorksite" class="btn btn-secondary">{{ $t('actions.history') }}</button>-->
-          <!--<button @click="contactOrg" class="btn btn-secondary">Contact</button>-->
-        </div>
+<div>
+  <div class="row">
+    <div class="col">
+      <div class="mx-auto" role="group">
+        <button id="newSiteBtn" @click="enterNewSite" class="btn btn-primary">{{ $t('actions.new') }}</button>
+        <button type="button" v-show="getWorksiteViews && getWorksiteViews.editWorksite" @click="saveForm" id="save-btn-top" class="btn btn-secondary">{{ $t('actions.save') }}</button>
+        <!--<b-btn id="printBtn" v-b-modal.modal1>Print</b-btn>-->
+        <button id="claim-btn" @click="fireClaimBtn" class="btn btn-secondary" v-show="isCurrentSiteClaimedByUserOrg || !isCurrentSiteClaimed" v-text="!isCurrentSiteClaimed || isCurrentSiteClaimed === null ? $t('actions.claim') : $t('actions.unclaim')"></button>
+        <!--<button id="historyBtn" v-show="getWorksiteViews && getWorksiteViews.editWorksite" class="btn btn-secondary">{{ $t('actions.history') }}</button>-->
+        <!--<button @click="contactOrg" class="btn btn-secondary">Contact</button>-->
       </div>
-      <PrintWorksite />
     </div>
+    <PrintWorksite />
   </div>
+</div>
 </template>
-<script>
-  import PrintWorksite from './PrintWorksite';
-  import {mapGetters} from 'vuex';
-  export default {
-    data() {
-      return {
-        status: "Open, unassigned"
-      }
-    },
-    components: {
-      PrintWorksite
-    },
-    computed: {
-      ...mapGetters(['isCurrentSiteClaimed', 'isCurrentSiteClaimedByUserOrg', 'getWorksiteViews'])
-    },
-    methods: {
-      enterNewSite() {
-        this.$store.commit('setActiveWorksiteView', {view: 'editWorksite'});
-        this.$store.commit('resetCurrentSiteData');
-        this.$store.commit('setSiteFormErrors', {})
-      },
-      saveForm() {
-        this.$store.dispatch('saveSite');
-      },
-      contactOrg() {
-        console.log("Contact org");
-      },
-      firePrintBtn() {
-        console.log("Print")
-      },
-      fireEditBtn() {
-        console.log("Load Form")
-      },
-      fireHistoryBtn() {
-        console.log("Load History")
-      },
-      fireClaimBtn() {
-        if (this.isCurrentSiteClaimed) {
-          this.$store.dispatch('unclaimSite');
-        } else {
-          this.$store.dispatch('claimSite');
-        }
-        // TODO: Update marker (emit/on or vuex computed prop)
-//        this.marker.setIcon(generateMarkerImagePath(this.claimedBy, this.status, this.workType));
-      },
-      statusChanged(event) {
-        const patchedSite = {
-          status: this.status
-        };
-        this.$http.patch(`/worksites/${this.siteId}`, patchedSite).then(resp => {
-          const data = resp.data;
-          this.claimedBy = data.claimedBy;
-          this.status = data.status;
-          this.marker.setIcon(generateMarkerImagePath(this.claimedBy, this.status, this.workType));
-        });
 
+
+<script>
+import PrintWorksite from './PrintWorksite';
+import {
+  mapGetters
+} from 'vuex';
+export default {
+  data() {
+    return {
+      status: "Open, unassigned"
+    }
+  },
+  components: {
+    PrintWorksite
+  },
+  computed: {
+    ...mapGetters(['isCurrentSiteClaimed', 'isCurrentSiteClaimedByUserOrg', 'getWorksiteViews'])
+  },
+  methods: {
+    enterNewSite() {
+      this.$store.commit('setActiveWorksiteView', {
+        view: 'editWorksite'
+      });
+      this.$store.commit('resetCurrentSiteData');
+      this.$store.commit('setSiteFormErrors', {})
+    },
+    saveForm() {
+      this.$notify({
+        group: 'foo',
+        title: 'Saving...',
+        //text: '',
+        position: 'top left',
+        width: 500
+      });
+      this.$store.dispatch('saveSite').then(() => {
+        console.log(this.$store.getters.getSiteFormErrors);
+        if (!this.$store.getters.getSiteFormErrors) {
+          this.$notify({
+            type: 'notification.success',
+            group: 'foo',
+            title: 'Success!',
+            text: 'Worksite saved.',
+            position: 'bottom center',
+            width: 500
+          });
+        } else {
+          this.$notify({
+            group: 'foo',
+            type: 'notification.error',
+            title: 'Just a second!',
+            text: 'We need a little more information.',
+            position: 'bottom center',
+            animation: 'Velocity',
+            width: 1000,
+            duration: 1000,
+            speed: 2000,
+            //clean: true
+          });
+        }
+      })
+    },
+    contactOrg() {
+      console.log("Contact org");
+    },
+    firePrintBtn() {
+      console.log("Print")
+    },
+    fireEditBtn() {
+      console.log("Load Form")
+    },
+    fireHistoryBtn() {
+      console.log("Load History")
+    },
+    fireClaimBtn() {
+      if (this.isCurrentSiteClaimed) {
+        this.$store.dispatch('unclaimSite');
+      } else {
+        this.$store.dispatch('claimSite');
       }
+      // TODO: Update marker (emit/on or vuex computed prop)
+      //        this.marker.setIcon(generateMarkerImagePath(this.claimedBy, this.status, this.workType));
+    },
+    statusChanged(event) {
+      const patchedSite = {
+        status: this.status
+      };
+      this.$http.patch(`/worksites/${this.siteId}`, patchedSite).then(resp => {
+        const data = resp.data;
+        this.claimedBy = data.claimedBy;
+        this.status = data.status;
+        this.marker.setIcon(generateMarkerImagePath(this.claimedBy, this.status, this.workType));
+      });
+
     }
   }
+}
 </script>
