cfc2b9bdd32b00edc3ebce290bd4556effe8e23e
Aaron Titus <aaron@crisiscleanup.org>
2018-06-11 13:22:18 -0600

    Adopt About.vue changes

--- src/views/pages/About.vue
+++ src/views/pages/About.vue
@@ -7,48 +7,48 @@
             <div class="col-md-12">
               <div class="row subheader">
                 <div class="small-12 medium-8 large-10 columns">
-                  <h1 v-html="aboutHeader"></h1>
-                  <h4 class="basic" v-html="philosophyNotTech"></h4>
+                  <h1 v-html="about.about_header"></h1>
+                  <h4 class="basic" v-html="about.philosophy_not_tech"></h4>
                   <hr class="medium">
-                  <p><strong v-html="mission"></strong></p>
+                  <p><strong v-html="about.mission"></strong></p>
                 </div>
               </div>
             </div>
             <div class="row">
               <div class="small-12 medium-12 large-12 columns">
                 <iframe width="800" height="400" src="https://www.youtube.com/embed/yCxI5YHyX5k" frameborder="0" allowfullscreen></iframe>
-                <p v-html="helpMorePeople"></p>
-                <p v-html="ccuBenefits"></p>
-                <p v-html="fourCsRs"></p>
-                <p v-html="propertyNotPeople"></p>
+                <p v-html="about.help_more_people"></p>
+                <p v-html="about.ccu_benefits"></p>
+                <p v-html="about.four_cs_rs"></p>
+                <p v-html="about.propert_not_people"></p>
 
-                <h5 v-html="transparentCollaborativePrivacy"></h5>
-                <p v-html="ccuLlcOpenSource"></p>
+                <h5 v-html="about.transparent_collaborative_privacy"></h5>
+                <p v-html="about.ccu_llc_open_source"></p>
 
-                <h4 v-html="foundationalPhilosophies"></h4>
+                <h4 v-html="about.foundational_philosophies"></h4>
                 <ul>
-                  <li v-html="rightWayGetItDone"></li>
-                  <li v-html="techEnhanceRelationships"></li>
-                  <li v-html="noPyramid"></li>
-                  <li v-html="collabCommConvenientRequired"></li>
-                  <li v-html="oneAppRuleAll"></li>
-                  <li v-html="dataMinimization"></li>
+                  <li v-html="about.rightWay_get_it_done"></li>
+                  <li v-html="about.tech_enhance_relationships"></li>
+                  <li v-html="about.no_pyramid"></li>
+                  <li v-html="about.collab_comm_convenient_required"></li>
+                  <li v-html="about.one_app_rule_all"></li>
+                  <li v-html="about.data_minimization"></li>
                 </ul>
-                <p v-html="innovationIsCoordination"></p>
-                <h3 v-html="testimonials"></h3>
+                <p v-html="about.innovation_is_coordination"></p>
+                <h3 v-html="about.testimonials"></h3>
                 <iframe width="800" height="400" src="https://www.youtube.com/embed/yhfWOPoY5hk" frameborder="0" allowfullscreen></iframe>
-                <h3 v-html="helpfulLinks"></h3>
+                <h3 v-html="about.helpful_links"></h3>
                 <ul>
-                  <li><a href="https://crisiscleanup-demo.herokuapp.com/" v-html="ccuDemo"></a></li>
-                  <li><a href="/training" v-html="mandatoryTraining"></a></li>
-                  <li><a href="https://crisiscleanup.zendesk.com/hc/en-us/articles/200603007-Requirements-for-Participation" v-html="reqForParticipation"></a></li>
-                  <li><a href="https://crisiscleanup.zendesk.com/hc/en-us/articles/200596493-Is-Crisis-Cleanup-a-Good-Fit-" v-html="goodFit"></a> <span v-html="factorsSuccess"></span>
+                  <li><a href="https://crisiscleanup-demo.herokuapp.com/" v-html="about.ccu_demo"></a></li>
+                  <li><a href="/training" v-html="about.mandatory_training"></a></li>
+                  <li><a href="https://crisiscleanup.zendesk.com/hc/en-us/articles/200603007-Requirements-for-Participation" v-html="about.req_for_participation"></a></li>
+                  <li><a href="https://crisiscleanup.zendesk.com/hc/en-us/articles/200596493-Is-Crisis-Cleanup-a-Good-Fit-" v-html="about.good_fit"></a> <span v-html="about.factors_success"></span>
                   </li>
                   <!--<li><a href="https://www.crisiscleanup.org/donate">{{ $t('about.contribute') }}</a></li>-->
-                  <li><a href="https://crisiscleanup.zendesk.com/hc/en-us" v-html="helpdesk"></a></li>
+                  <li><a href="https://crisiscleanup.zendesk.com/hc/en-us" v-html="about.helpdesk"></a></li>
                 </ul>
-                <h3 v-html="ccuStory"></h3>
-                <p v-html="h2PrepLink"></p>
+                <h3 v-html="about.ccu_story"></h3>
+                <p v-html="about.h2Prep_link"></p>
               </div>
             </div>
           </div>
@@ -63,33 +63,35 @@
     name: 'About',
     data() {
       return {
-        aboutHeader: this.$i18n.t('about.about_header'),
-        philosophyNotTech: this.$i18n.t('about.philosophy_not_tech'),
-        mission: this.$i18n.t('about.mission'),
-        helpMorePeople: this.$i18n.t('about.help_more_people'),
-        ccuBenefits: this.$i18n.t('about.ccu_benefits'),
-        fourCsRs: this.$i18n.t('about.four_cs_rs'),
-        propertyNotPeople: this.$i18n.t('about.property_not_people'),
-        transparentCollaborativePrivacy: this.$i18n.t('about.transparent_collaborative_privacy'),
-        ccuLlcOpenSource: this.$i18n.t('about.ccu_llc_open_source'),
-        foundationalPhilosophies: this.$i18n.t('about.foundational_philosophies'),
-        rightWayGetItDone: this.$i18n.t('about.right_way_get_it_done'),
-        techEnhanceRelationships: this.$i18n.t('about.tech_enhance_relationships'),
-        noPyramid: this.$i18n.t('about.no_pyramid'),
-        collabCommConvenientRequired: this.$i18n.t('about.collab_comm_convenient_required'),
-        oneAppRuleAll: this.$i18n.t('about.one_app_rule_all'),
-        dataMinimization: this.$i18n.t('about.data_minimization'),
-        innovationIsCoordination: this.$i18n.t('about.innovation_is_coordination'),
-        testimonials: this.$i18n.t('about.testimonials'),
-        helpfulLinks: this.$i18n.t('about.helpful_links'),
-        ccuDemo: this.$i18n.t('about.ccu_demo'),
-        mandatoryTraining: this.$i18n.t('about.mandatory_training'),
-        reqForParticipation: this.$i18n.t('about.req_for_participation'),
-        goodFit: this.$i18n.t('about.good_fit'),
-        factorsSuccess: this.$i18n.t('about.factors_success'),
-        helpdesk: this.$i18n.t('about.helpdesk'),
-        ccuStory: this.$i18n.t('about.ccu_story'),
-        h2PrepLink: this.$i18n.t('about.h2prep_link')
+        about: {
+          about_header: this.$t('about.about_header'),
+          philosophy_not_tech: this.$t('about.philosophy_not_tech'),
+          mission: this.$t('about.mission'),
+          help_more_people: this.$t('about.help_more_people'),
+          ccu_benefits: this.$t('about.ccu_benefits'),
+          four_cs_rs: this.$t('about.four_cs_rs'),
+          property_not_people: this.$t('about.property_not_people'),
+          transparent_collaborative_privacy: this.$t('about.transparent_collaborative_privacy'),
+          ccu_llc_open_source: this.$t('about.ccu_llc_open_source'),
+          foundational_philosophies: this.$t('about.foundational_philosophies'),
+          right_way_get_it_done: this.$t('about.right_way_get_it_done'),
+          tech_enhance_relationships: this.$t('about.tech_enhance_relationships'),
+          no_pyramid: this.$t('about.no_pyramid'),
+          collab_comm_convenient_required: this.$t('about.collab_comm_convenient_required'),
+          one_app_rule_all: this.$t('about.one_app_rule_all'),
+          data_minimization: this.$t('about.data_minimization'),
+          innovation_is_coordination: this.$t('about.innovation_is_coordination'),
+          testimonials: this.$t('about.testimonials'),
+          helpful_links: this.$t('about.helpful_links'),
+          ccu_demo: this.$t('about.ccu_demo'),
+          mandatory_training: this.$t('about.mandatory_training'),
+          req_for_participation: this.$t('about.req_for_participation'),
+          good_fit: this.$t('about.good_fit'),
+          factors_success: this.$t('about.factors_success'),
+          helpdesk: this.$t('about.helpdesk'),
+          ccu_story: this.$t('about.ccu_story'),
+          h2prep_link: this.$t('about.h2prep_link')
+        }
       }
     },
     components: {},
