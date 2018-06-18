2901f5949d84b9c460fffb0aabbc9ccf88b12d86
Mykolas Kvieska <mykolaska@outlook.com>
2018-06-18 06:37:01 +0300

    add constant for the size limit

--- test/unit/specs/components/sites/ImageSize.spec.js
+++ test/unit/specs/components/sites/ImageSize.spec.js
@@ -1,5 +1,7 @@
 import readDirFiles from 'read-dir-files';
 
+const FILE_SIZE_LIMIT = 300; // KB
+
 var checkSizeOfAllImages = function() {
   return new Promise(resolve => {
     let count = 0;
@@ -14,13 +16,12 @@ var checkSizeOfAllImages = function() {
           const fs = require('fs'); //Load the filesystem module
           const stats = fs.statSync(filename);
           const fileSize = stats.size / 1024;
-          if (fileSize >= 300) {
+          if (fileSize >= FILE_SIZE_LIMIT) {
             count++;
             console.log(`${filename} is over the Limit.`);
           }
         }
       }
-
       resolve(count);
     });
   });
