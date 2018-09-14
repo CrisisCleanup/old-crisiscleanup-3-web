7535907ececeb7680d6dd4c32790f4e223dc0441
Mykolas Kvieska <mykolaska@outlook.com>
2018-09-13 23:30:21 -0400

    Fix issue for unit test

--- config/development/dev.env.js
+++ config/development/dev.env.js
@@ -3,7 +3,5 @@ var prodEnv = require('../production/prod.env');
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  LEAFLET_MAP_APP_ID: '"OxegEAQ46ansqN9LtNd1"',
-  LEAFLET_MAP_APP_CODE: '"86XV8M-pIbpy5TUa9H_Aww"',
   API_ENDPOINT: '"https://dev-api.crisiscleanup.services"'
 });
